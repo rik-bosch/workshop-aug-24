9eeef3a2ebedbb31107bea6f1513ff4b7e6e59cc
Rik Bosch <rbosch@quintor.nl>
2024-08-01 12:10:32 +0200

    Opdracht 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+    public static int celsiusToFahrenheit(int celsius) {
+        return celsius * 9 / 5 + 32;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] degrees = {-10, 0, 25, 30, 50, 100};
+        for (int degree: degrees) {
+            System.out.printf(
+                    "%d graden celsius is %d graden fahrenheit\n",
+                    degree,
+                    celsiusToFahrenheit(degree)
+            );
+        }
     }
 }
