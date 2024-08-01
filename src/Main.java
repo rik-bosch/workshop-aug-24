259bf0a22ad29a13652f2471d4e7e725c5510ed5
Rik Bosch <rbosch@quintor.nl>
2024-08-01 12:52:24 +0200

    Opdracht 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static int celsiusToFahrenheit(int celsius) {
-        return celsius * 9 / 5 + 32;
+        // FIXME: algorithm is broken for integers below 0.
+        int t1 = celsius << 3;
+        int t2 = t1 + celsius;
+        int t3 = 0;
+        for (int t4 = 8; t4 > 0; t4 = t4 - 1) {
+            int t5 = 5 << t4;
+            t2 = t2 - t5;
+            if (t2 >= 0) {
+                int t6 = (1 << t4);
+                t3 = t3 + t6;
+            }
+            else {
+                t2 = t2 + t5;
+            }
+        }
+        int t7 = t3 + 32;
+        return t7;
     }
 
     public static void main(String[] args) {
