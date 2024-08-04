fb2f951a98d1aff7d46a9e29a54ca3adf9b6bb61
Rik Bosch <rbosch@quintor.nl>
2024-08-04 08:56:15 +0200

    Opdracht 3.2 zonder alle exception handling.

--- src/WorkshopApp.java
+++ src/WorkshopApp.java
@@ -10,51 +10,33 @@ public class WorkshopApp {
             0x21, 0x0a
     };
 
-    public void helloWorld(OutputStream out) {
-        try {
+    public void helloWorld(OutputStream out) throws Exception {
             for (int i = 0; i < message.length; ++i) {
                 out.write(message[i]);
             }
-        }
-        catch (IOException e) {
-            System.err.println("Error writing to output.");
-            System.err.println(e.getMessage());
-        }
     }
 
-    public void writeHttpHeaders(OutputStream out) {
+    public void writeHttpHeaders(OutputStream out) throws Exception {
         String headers = """
                 HTTP/1.1 200 OK
                 Content-Type: text/plain
                 
                 """;
 
-        try {
-            out.write(headers.getBytes());
-        }
-        catch (IOException e) {
-            System.err.println("Error writing bytes.");
-            System.err.println(e.getMessage());
-        }
+        out.write(headers.getBytes());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var app = new WorkshopApp();
+        System.out.println("Listening on: http://localhost:8080");
         try (
                 var server = new ServerSocket(8080);
                 Socket socket = server.accept();
                 OutputStream outputStream = socket.getOutputStream();
         ) {
-            System.out.println("Listening on: http://localhost:8080");
             app.writeHttpHeaders(outputStream);
             app.helloWorld(outputStream);
         }
-        catch (IOException e) {
-            System.err.println("Error opening server.");
-            System.err.println(e.getMessage());
-        }
-        finally {
-            System.out.println("My job is done.");
-        }
+        System.out.println("My job is done.");
     }
 }
